4e5534920e60566f4331abd222b9d7984b3e98a5
RyanVries <45096137+RyanVries@users.noreply.github.com>
2019-02-13 14:59:43 +0100

    Add files via upload

--- Classification_project.py
+++ Classification_project.py
@@ -5,8 +5,8 @@ Created by Ryan de Vries for project computational biology
 
 import pandas as pd
 import seaborn as sns
-import time
 import datetime
+import sklearn
 
 def read_data(file_loc):
     dframe=pd.read_csv(file_loc)
@@ -24,7 +24,6 @@ def remove_nan(dframe):
 def make_cluster(dframe,remove,save_fig,class_sort='lung_carcinoma'):
     if remove==True:
         dframe=remove_nan(dframe)
-        
     cla=dframe[class_sort]
     lut = dict(zip(cla.unique(), 'rbg'))
     labels=cla.unique()
@@ -39,12 +38,34 @@ def make_cluster(dframe,remove,save_fig,class_sort='lung_carcinoma'):
     g.ax_heatmap.set_title('Clustermap of the protein biomarkers with labels of the different known '+str(class_sort)+' classes')
     if save_fig==True:
         x=datetime.datetime.now()
-        ex='_'.join([str(x.hour),str(x.minute)])
-        g.savefig('clustermap'+ex+'.png')
+        extra='_'.join([str(x.hour),str(x.minute)])
+        g.savefig('clustermap'+extra+'.png')
     return
 
+def approach_paper(dframe,thresholds):
+    (rows,columns)=dframe.shape
+    LC_index=[];
+    for pat in range(0,rows):
+        for i in range(5,12):
+            TM=dframe.columns[i]
+            if TM in thresholds.keys() and pat not in LC_index:
+                if dframe.iloc[pat,i]>=thresholds[TM]:
+                    LC_index.append(pat)
+    return LC_index
+
+def compare_with_ground(dframe,prediction):
+    """under construction"""
+    clinical=dframe['primary_tumor']
+    clframe,kept=remove_nan(clinical)
+    con=sklearn.metrics.confusion_matrix(clframe,prediction[kept])
+    tn, fp, fn, tp = con([0, 1, 0, 1], [1, 1, 1, 0]).ravel()
+    
+    return
+    
 file_loc='tumormarkers_lungcancer.csv'
 dframe=read_data(file_loc)
 make_cluster(dframe=dframe, remove=True, save_fig=False, class_sort='lung_carcinoma')
 
-
+thresholds={'TM_CA15.3 (U/mL)': 35,'TM_CEA (ng/mL)':5,'TM_CYFRA (ng/mL)':3.3,'TM_NSE (ng/mL)':25,'TM_PROGRP (pg/mL)':50,'TM_SCC (ng/mL)':2}
+LC_paper=approach_paper(dframe,thresholds)
+compare_with_ground(dframe,LC_paper)
