bee9dcb6eec562843e3b397c963378ce8c851348
RyanVries <45096137+RyanVries@users.noreply.github.com>
2019-02-14 15:41:46 +0100

    Add files via upload

--- Classification_project.py
+++ Classification_project.py
@@ -5,8 +5,10 @@ Created by Ryan de Vries for project computational biology
 
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import datetime
 import sklearn
+from sklearn.metrics import confusion_matrix
 
 def read_data(file_loc):
     dframe=pd.read_csv(file_loc)
@@ -16,10 +18,24 @@ def remove_nan_dframe(dframe):
     drop_index=[];
     (r,c)=dframe.shape
     for i in range(0,r):
-        if isinstance(dframe.iloc[i,1], float) and isinstance(dframe.iloc[i,2], float):
+        if isinstance(dframe.iloc[i,1], float) and isinstance(dframe.iloc[i,2], float):  #kijken of dit een and of or moet worden 
             drop_index.append(i)
     dframe=dframe.drop(drop_index,axis=0)
     return dframe
+
+def remove_nan_list(lis):
+    drop_index=[];
+    kept=[];
+    r=len(lis)
+    for i in range(0,r):
+        if isinstance(lis[i], float): 
+            drop_index.append(i)
+        else:
+            kept.append(i)
+    print(type(drop_index))
+    for index in sorted(drop_index, reverse=True):
+        del lis[index]          
+    return lis, kept
     
 def make_cluster(dframe,remove,save_fig,class_sort='lung_carcinoma'):
     if remove==True:
@@ -51,14 +67,21 @@ def approach_paper(dframe,thresholds):
             if TM in thresholds.keys() and pat not in LC_index:
                 if dframe.iloc[pat,i]>=thresholds[TM]:
                     LC_index.append(pat)
-    return LC_index
+    LC_results=np.zeros(rows);
+    for k in range(0,rows):
+        if k in LC_index:
+            LC_results[k]=1
+    #0 means no cancer and 1 implies cancer
+    return LC_results
 
-def compare_with_ground(dframe,prediction):
+def compare_with_ground(dframe,prediction,category):
     """under construction"""
-    clinical=dframe['primary_tumor']
-    "clframe,kept=remove_nan(clinical)"
-    con=sklearn.metrics.confusion_matrix(clframe,prediction[kept])
-    tn, fp, fn, tp = con([0, 1, 0, 1], [1, 1, 1, 0]).ravel()
+    truth=dframe[category]
+    truth=truth.tolist()
+    truth, kept=remove_nan_list(truth)   #hier zit een error
+    #convert the strings to integers
+    #confusion_matrix(truth,prediction[kept])
+    #tn, fp, fn, tp = confusion_matrix([0, 1, 0, 1], [1, 1, 1, 0]).ravel()
     
     return
     
@@ -68,4 +91,4 @@ make_cluster(dframe=dframe, remove=True, save_fig=False, class_sort='lung_carcin
 
 thresholds={'TM_CA15.3 (U/mL)': 35,'TM_CEA (ng/mL)':5,'TM_CYFRA (ng/mL)':3.3,'TM_NSE (ng/mL)':25,'TM_PROGRP (pg/mL)':50,'TM_SCC (ng/mL)':2}
 LC_paper=approach_paper(dframe,thresholds)
-compare_with_ground(dframe,LC_paper)
+compare_with_ground(dframe,LC_paper,'primary_tumor')     #moet dit primary tumor zijn of lung carcinoma??
