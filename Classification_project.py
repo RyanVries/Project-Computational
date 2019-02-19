9ac779bb807561d6a36244d8536c3aa873475def
RyanVries <45096137+RyanVries@users.noreply.github.com>
2019-02-19 16:25:27 +0100

    Add files via upload

--- Classification_project.py
+++ Classification_project.py
@@ -11,16 +11,18 @@ from astropy.table import Table, Column
 from sklearn.metrics import confusion_matrix
 
 def read_data(file_loc):
+    '''read the desired data from the csv file as a dataframe'''
     dframe=pd.read_csv(file_loc)
     return dframe
 
 def remove_nan_dframe(dframe,class_sort):
+    '''remove patients from the dataframe which contain a Nan value in the specified column name and return the new dataframe and the original indexes which were kept '''
     drop_index=[];
     kept=[];
     (r,c)=dframe.shape
     column=dframe[class_sort]
     for i in range(0,r):
-        if isinstance(column[i], float):
+        if isinstance(column[i], float):    #a Nan is classified as a float in python
             drop_index.append(i)
         else:
             kept.append(i)
@@ -28,6 +30,7 @@ def remove_nan_dframe(dframe,class_sort):
     return dframe, kept
 
 def remove_nan_list(lis):
+    '''remove Nan values from the list and also specify which original indexes where kept '''
     drop_index=[];
     kept=[];
     r=len(lis)
@@ -36,40 +39,41 @@ def remove_nan_list(lis):
             drop_index.append(i)
         else:
             kept.append(i)
-    for index in sorted(drop_index, reverse=True):
+    for index in sorted(drop_index, reverse=True):  #elements need to be deleted from back to front to prevent indexing issues
         del lis[index]          
     return lis, kept
     
 def make_clustermap(dframe,remove,save_fig,class_sort='lung_carcinoma'):
-    if remove==True:
+    '''make a clustermap of the selected column in the dataframe together with the corresponding labels of each patient'''
+    if remove==True:    #remove Nan's if specified
         dframe, kept=remove_nan_dframe(dframe,class_sort)
     cla=dframe[class_sort]
     labels=cla.unique()
-    lut = dict(zip(labels, 'rbg'))
+    lut = dict(zip(labels, 'rbg')) #create dictionary of possible options
     row_colors = cla.map(lut)
     markers=dframe.iloc[:,[5,6,7,8,9,10,11]]
     g=sns.clustermap(markers, metric='correlation', method='single', col_cluster=False, row_colors=row_colors, z_score=0)
 
-    for label in labels:
+    for label in labels:     #add the labels of each patient next to the clustermap
         g.ax_col_dendrogram.bar(0, 0, color=lut[label],
                             label=label, linewidth=0)
-    g.ax_col_dendrogram.legend(loc='center', ncol=3)
+    g.ax_col_dendrogram.legend(loc='center', ncol=len(labels))
     g.ax_heatmap.set_title('Clustermap of the protein biomarkers with labels of the different known '+str(class_sort)+' classes')
-    if save_fig==True:
+    if save_fig==True:      #save the figure if wanted with a unique name to prevent overwriting files
         x=datetime.datetime.now()
-        extra='_'.join([str(x.day),str(x.hour),str(x.minute),str(x.second)])
+        extra='_'.join([str(x.year),str(x.month),str(x.day),str(x.hour),str(x.minute),str(x.second)])
         g.savefig('clustermap'+extra+'.png')
     return
 
 def approach_paper(dframe,thresholds):
-    """integer 0 implies that the patient is not expected to have a carcinoma and label 1 implies an expected carcinoma"""
+    """use the specified thresholds from the paper to classify each patient (LC=1 and no LC=0)"""
     (rows,columns)=dframe.shape
     LC_index=[];
     LC_results=[];
     for pat in range(0,rows):
         for i in range(5,12):
             TM=dframe.columns[i]
-            if TM in thresholds.keys() and pat not in LC_index:
+            if TM in thresholds.keys() and pat not in LC_index:     #see if a threshold is present for the tumor marker and if the patient is not already classified as having LC
                 if dframe.iloc[pat,i]>=thresholds[TM]:
                     LC_index.append(pat)
                     LC_results.append(1)
@@ -79,14 +83,13 @@ def approach_paper(dframe,thresholds):
     return LC_results
 
 def compare_with_ground_basic(dframe,prediction,category):
-    """under development"""
+    '''Evaluate the predictions by comparing them with the ground truth and calculate the desired statistical values'''
     frame, kept=remove_nan_dframe(dframe,category)
     tru=frame[category]
     pred_down=[];
-    for z in range(0,len(prediction)):
+    for z in range(0,len(prediction)):   #only keep the prediction if the true value is known
         if z in kept:
             pred_down.append(prediction[z])
-    
     labels=['Yes','No']
     lut = dict(zip(labels, [1,0]))
     ground = tru.map(lut)
@@ -103,6 +106,7 @@ def compare_with_ground_basic(dframe,prediction,category):
     return PPV,NPV,sensitivity,specificity, cnf_matrix
 
 def print_stats(PPV,NPV,sensi,speci):
+    'Make a tabel of the relevant statistical values and print this'''
     values='{:.2f} {:.2f} {:.2f} {:.2f}'.format(100*PPV,100*NPV,100*sensi,100*speci)
     values=values.split()
     t=Table([[float(values[0])],[float(values[1])],[float(values[2])],[float(values[3])]],names=('PPV (%)','NPV (%)','Sensitivity (%)','Specificity (%)'),meta={'name':'Statistical values'})
